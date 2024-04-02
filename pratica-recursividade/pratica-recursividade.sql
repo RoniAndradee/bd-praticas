3b4fc080e719717576abc9fedd75b895e2374101
roniandradee <ronielli.santos@sptech.school>
2024-04-01 22:30:25 -0300

    terminando recursividade

--- pratica-recursividade/pratica-recursividade.sql
+++ pratica-recursividade/pratica-recursividade.sql
@@ -1,6 +1,13 @@
 create database recursividade;
 use recursividade;
 
+-- criação da tabela dono
+create table dono (
+idDono int primary key auto_increment,
+nome varchar(45),
+bairro varchar(50),
+telefone varchar(20));
+
 -- criação da tabela pet
 create table pet(
 idPet int primary key auto_increment,
@@ -19,13 +26,6 @@ fkDono int not null,
 	references dono(idDono)
 );
 
--- criação da tabela dono
-create table dono (
-idDono int primary key auto_increment,
-nome varchar(45),
-bairro varchar(50),
-telefone varchar(20));
-
 -- inserindo dados na tabela dono
 insert into dono values 
 (default, 'Roni', 'Taipas', '11977336964'),
@@ -41,21 +41,18 @@ insert into pet (idPet, nome, pedigree, raca, fkDono) values
 update pet set fkPai = 2 where idPet = 1;
 update pet set fkMae = 3 where idPet = 1;
 
--- selecionando o nome do pet, o nome do seu pai o nome de sua mãe e o nome do seu dono
+-- selecionando o nome do pet, o nome do seu pai o nome de sua mãe, o nome do seu dono e o telefone do dono
 select filho.nome as PetFilho,
 pai.nome as PetPai,
 mae.nome as petMae,
-dono.nome as nomeDono
+dono.nome as nomeDono,
+dono.telefone as TelefoneDoDono
 from pet as filho
 join pet as pai on filho.fkPai = pai.idPet
 join pet as mae on filho.fkMae = mae.idPet
-join dono as dono on filho.fkDono = dono.idDono;
-
-select * from pet;
-select * from 
+join dono on filho.fkDono = dono.idDono;
 
 -- tabela motorista e carro
-
 -- criação da tabela carro
 create table carro(
 idCarro int primary key auto_increment,
@@ -99,16 +96,15 @@ update motorista set fkCarro = 1 where idMotorista = 1;
 update motorista set fkCarro = 3 where idMotorista = 2;
 update motorista set fkCarro = 2 where idMotorista = 3;
 
--- selecionando o nome do motorista o nome do seu motorista reserva e o nome do seu carro
-select motorista.nome as NomeMotorista,
-reserva.nome as MotoristaReserva,
-carro.nome as Carro
+-- selecionando o nome do motorista, o nome do seu carro, a marca do seu carro, o nome do seu motorista reserva
+select motorista.nome as NomeDoMotorista, 
+carro.nome as CarroDoMotorista, carro.marca as MarcaDoCarro,
+reserva.nome as NomeDoReserva
 from motorista
 join motorista as reserva on motorista.fkMotoristaReserva = reserva.idMotorista
 join carro on motorista.fkCarro = carro.idCarro;
 
 -- tabelas empresa e funcionário
-
 -- criação da tablea empresa 
 create table empresa(
 idEmp int primary key auto_increment,
@@ -146,12 +142,12 @@ update empresa set fkMatriz = 2 where idEmp = 2;
 
 -- inserindo dados na tabela funcionario
 insert into funcionario (idFunc, nome, area, salario) values
-(default, 'Fernando', 'Borracharia', 4.550),
-(default, 'Kalleb', 'Borracharia', 6.800),
-(default, 'Zézinho', 'Elétrica', 4.200),
-(default, 'Zezão', 'Elétrica', 6.100),
-(default, 'Homem Pedra', 'Marketing', 5.800),
-(default, 'Pardinho', 'Marketing', 7.800);
+(default, 'Fernando', 'Borracharia', 4550.00),
+(default, 'Kalleb', 'Borracharia', 6800.00),
+(default, 'Zézinho', 'Elétrica', 4200.00),
+(default, 'Zezão', 'Elétrica', 6100.00),
+(default, 'Homem Pedra', 'Marketing', 5800.00),
+(default, 'Pardinho', 'Marketing', 7800.00);
 
 -- atualizando o supervisor de cada funcionario
 update funcionario set fkSupervisor = 2 where idFunc = 1;
@@ -163,26 +159,30 @@ update funcionario set fkEmpresa = 1 where idFunc in(1,2);
 update funcionario set fkEmpresa = 2 where idFunc in(3,4);
 update funcionario set fkEmpresa = 3 where idFunc in(5,6);
 
--- select com join
-select funcionario.nome as NomeFuncionario,
-supervisor.nome as SupervisorDoFuncionario,
-empresa.nome as Empresa
+-- selecionando o nome do funcionario, seu salário, nome do seu supervisor, seu salário, a empresa em que ele trabalha e a matriz da empresa que ele trabalha
+select funcionario.idFunc, funcionario.nome as NomeDoFuncionario, funcionario.salario as SalarioDoFuncionario,
+supervisor.nome as NomeDoSupervisor, supervisor.salario as SalarioDoSupervisor,
+empresa.nome as EmpresaQueTrabalha, matriz.nome as MatrizDaEmpresa
+ from funcionario
+join funcionario as supervisor on funcionario.fkSupervisor = supervisor.idFunc
+join empresa as empresa on funcionario.fkEmpresa = empresa.idEmp
+join empresa as matriz on empresa.fkMatriz = matriz.idEmp;
+
+/*
+selecionando o nome do funcionario, a area que ele trabalha, o salario dele, o nome do supervisor dele, a area que o supervisor trabalha,
+o salario do supervisor e a empresa que eles tabalham e a matriz dessa empresa
+*/
+select funcionario.nome as NomeFunc, funcionario.area as Area, funcionario.salario as SalarioFunc,
+supervisor.nome as NomeSupervisor, supervisor.area as Area, supervisor.salario as SalarioSupervisor,
+empresa.nome as nomeDaEmpresa, 
+matriz.nome as MatrizDaEmpresa
 from funcionario
 join funcionario as supervisor on funcionario.fkSupervisor = supervisor.idFunc
-join empresa on funcionario.fkEmpresa = empresa.idEmp;
+join empresa as empresa on funcionario.fkEmpresa = empresa.idEmp
+join empresa as matriz on empresa.fkMatriz = matriz.idEmp;
 
-select funcionario.nome as NomedoFuncionarios,
-funcionario.area as Area,
-salario.valor as Salário,
-supervisor.nome as NomeDoSupervisor,
-empresa.nome as NomeDaEmpresa
-from funcionario
-join funcionario as salario on funcionario.salario = salario.idFunc
-join funcionario as supervisor on funcionario.supervisor = supervisor.idFunc
-join empresa on fkEmpresa = idEmp;
-
- 
-select empresa.nome as NomeDaEmpresa,
-matriz.nome as MatrizDaEmpresa
+-- selecionando o nome da empresa e a sua matriz 
+select empresa.nome,
+matriz.nome 
 from empresa 
 join empresa as matriz on empresa.fkMatriz = matriz.idEmp;
