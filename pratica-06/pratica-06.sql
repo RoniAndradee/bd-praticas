ac257f8494e5f14cc1eb614cdff2e1914dd96068
roniandradee <ronielli.santos@sptech.school>
2024-04-15 22:52:42 -0300

    terminado pratica 06

--- pratica-06/pratica-06.sql
+++ pratica-06/pratica-06.sql
@@ -1,5 +1,4 @@
--- exercício 01 
-
+-- Exercício 01 
 create database pratica06;
 use pratica06;
 
@@ -28,7 +27,9 @@ idAcompanhante int,
 fkAluno char(8),
 constraint pkAcompanhanteAluno primary key (idAcompanhante, fkAluno),
 nome varchar(45),
-tipoRelacao varchar(45)
+tipoRelacao varchar(45),
+constraint fkAcompanhanteAluno foreign key (fkAluno)
+	references aluno (ra)
 );
 
 -- Inserir dados nas tabelas.
@@ -69,7 +70,7 @@ select * from aluno join projeto on fkProjeto = idProjeto where projeto.nome = '
 -- Exibir os dados dos alunos, dos projetos correspondentes e dos seus acompanhantes.
 select * from aluno join projeto on fkProjeto = idProjeto left join acompanhante on fkAluno = ra;
 
--- exercicio 02
+-- Exercício 02
 
 -- Criar um banco de dados chamado Campanha.
 create database campanha;
@@ -92,30 +93,57 @@ constraint fkOrganizadorOrientador foreign key (fkOrientador)
 create table campanha(
 idCampanha int,
 fkOrganizador int,
-
-categoria varchar(45),
-instituicao varchar(45),
+constraint pkCampanhaOrganizador primary key (idCampanha, fkOrganizador),
+categoriaDoacao varchar(45),
+instituicao1 varchar(45),
 instituicao2 varchar(45),
-dtFinal date
+dtFinal date,
+constraint fkCampanhaOrganizador foreign key (fkOrganizador)
+	references organizador (idOrganizador)
 );
 
-alter table campanha auto_increment = 500;
-
 -- Inserir dados nas tabelas, de forma que exista mais de uma campanha para algum organizador, e mais de um organizador novato sendo orientado por algum organizador mais experiente
-insert into organizador (idOrganizador, nome, rua, bairro, email) values 
-(default, 'Miguel', 'Rua Pelicano', 'Padre Ulrico', 'miguel2022@gmail.com'),
-(default, 'Ivan', 'Rua Major Gavínio Viana', 'Cidade Nova', 'ivanbixao@provedor.com'),
-(default, 'Jasmin', 'Rua Juscelino Kubitschek', 'Ponto Novo','jasmincrazy@taligado.com'),
-(default, 'Chico', 'Rua Cristóvão Coelho', 'Mecejana','chicao@gmail.com'),
-(default, 'Suellen', 'Rua Arthur Bernardes', 'São Geraldo','suellen-atrasos@gmail.com'),
-(default, 'Adriano', 'Rua José Alberto Nunes', 'São João','adrizinho@gmail.com');
+insert into organizador values 
+(default, 'Miguel', 'Rua Pelicano', 'Padre Ulrico', 'miguel2022@gmail.com', null),
+(default, 'Ivan', 'Rua Major Gavínio Viana', 'Cidade Nova', 'ivanbixao@provedor.com', null),
+(default, 'Jasmin', 'Rua Juscelino Kubitschek', 'Ponto Novo','jasmincrazy@taligado.com', 30),
+(default, 'Chico', 'Rua Cristóvão Coelho', 'Mecejana','chicao@gmail.com', 30),
+(default, 'Suellen', 'Rua Arthur Bernardes', 'São Geraldo','suellen-atrasos@gmail.com', 31),
+(default, 'Adriano', 'Rua José Alberto Nunes', 'São João','adrizinho@gmail.com', 31);
 
 insert into campanha values 
-(default, 'alimento', 'AACD', 'CAPS', '2023-12-15', 30),
-(default, 'produto de higiene', 'Associação Peter Pan', null, '2024-02-12', 31),
-(default, 'racao', 'Lar TinTin', null, '2024-01-26', 32),
-(default, 'medicamento', 'ABRACE', null, '2023-08-02', 33),
-(default, 'alimento', 'APAE', 'CAPS', '2022-10-18', 34),
-(default, 'produto de limpeza', 'Casa do Zezinho', 'Cruz Vermelha', '2024-03-29', 35),
-(default, 'roupa', 'criança esperança', null, '2023-08-15', 30),
-(default, 'material de pintura', 'ICA', null, '2022-05-02', 31);
+(1, 30, 'alimento', 'AACD', 'CAPS', '2023-12-15'),
+(1, 31, 'produto de higiene', 'Associação Peter Pan', null, '2024-02-12'),
+(1, 32,'racao', 'Lar TinTin', null, '2024-01-26'),
+(1, 33, 'medicamento', 'ABRACE', null, '2023-08-02'),
+(1, 34, 'alimento', 'APAE', 'CAPS', '2022-10-18'),
+(1, 35, 'produto de limpeza', 'Casa do Zezinho', 'Cruz Vermelha', '2024-03-29'),
+(2, 30, 'roupa', 'criança esperança', null, '2023-08-15'),
+(2, 31,'material de pintura', 'ICA', null, '2022-05-02');
+
+-- Exibir todos os dados de cada tabela criada, separadamente.
+select * from organizador;
+select * from campanha;
+
+-- Exibir os dados dos organizadores e os dados de suas respectivas campanhas.
+select * from organizador join campanha on fkOrganizador = idOrganizador;
+
+-- Exibir os dados de um determinado organizador (informar o nome do organizador na consulta) e os dados de suas respectivas campanhas.
+select * from organizador join campanha on fkOrganizador = idOrganizador where organizador.nome = 'Ivan';
+
+-- Exibir os dados dos organizadores novatos e os dados dos respectivos organizadores orientadores.
+select * from organizador as novato join organizador as orientador on novato.fkOrientador = orientador.idOrganizador;
+
+/*Exibir os dados dos organizadores novatos e os dados dos respectivos
+organizadores orientadores, porém somente de um determinado organizador
+orientador (informar o nome do organizador orientador na consulta).*/
+select * from organizador as novato join organizador as orientador on novato.fkOrientador = orientador.idOrganizador where orientador.nome = 'Miguel';
+
+-- Exibir os dados dos organizadores novatos, os dados das respectivas campanhas organizadas e os dados dos respectivos organizadores orientadores.
+select * from organizador as novato join campanha on campanha.fkOrganizador = novato.idOrganizador join organizador as orientador on novato.fkOrientador = orientador.idOrganizador;
+
+/*Exibir os dados de um organizador novato (informar o seu nome na consulta),
+os dados das respectivas campanhas em que trabalha e os dados do seu
+organizador orientador.*/
+select * from organizador as novato join campanha on campanha.fkOrganizador = novato.idOrganizador 
+join organizador as orientador on novato.fkOrientador = orientador.idOrganizador where novato.nome = 'Chico';
